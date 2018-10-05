fb472800927f3840c54a365ab9c646d608dd58f3
desjoerd <sjoerd.meer@outlook.com>
2018-10-05 11:16:55 +0200

    Initial commit

--- JsonRounding/Program.cs
+++ JsonRounding/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace JsonRounding
 {
@@ -6,7 +7,67 @@ namespace JsonRounding
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var target = new Target();
+
+            var settings = new JsonSerializerSettings();
+            settings.Converters.Add(new RoundingJsonConverter(2));
+
+            var result = JsonConvert.SerializeObject(target, settings);
+
+            Console.WriteLine(result);
+            Console.Read();
+        }
+    }
+
+    public class Target
+    {
+        public Target()
+        {
+            PI = (decimal)Math.PI;
+        }
+
+        public decimal PI { get; set; } 
+    }
+
+    public class RoundingJsonConverter : JsonConverter
+    {
+        int _precision;
+        MidpointRounding _rounding;
+
+        public RoundingJsonConverter()
+            : this(2)
+        {
+        }
+
+        public RoundingJsonConverter(int precision)
+            : this(precision, MidpointRounding.AwayFromZero)
+        {
+        }
+
+        public RoundingJsonConverter(int precision, MidpointRounding rounding)
+        {
+            _precision = precision;
+            _rounding = rounding;
+        }
+
+        public override bool CanRead
+        {
+            get { return false; }
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(decimal);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue(Math.Round((decimal)value, _precision, _rounding));
         }
     }
 }
